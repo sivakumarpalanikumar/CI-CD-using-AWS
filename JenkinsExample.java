efaad831ffa67ee25db1287860c668f394723210
Sivakumar P <72782266+sivakumarpalanikumar@users.noreply.github.com>
2023-07-09 17:12:23 +0530

    Update JenkinsExample.java

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -2,7 +2,7 @@ public class JenkinsExample {
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
-    System.out.println("Hello Worldd");
+    System.out.println("Hello Worlddd");
 
  }
 
